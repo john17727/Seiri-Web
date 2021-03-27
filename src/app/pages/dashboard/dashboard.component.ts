80f20b5bfbd00504529504b85d28fb1eba2d2157
john17727 <justanotherone17727@gmail.com>
2021-03-27 17:42:59 -0600

    Added header to the dashboard page.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  today: Date = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
